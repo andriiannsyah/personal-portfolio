72e222b4ff33ecf179cb84e76eea1b6dec6bed04
andriansyah <anandri394@gmail.com>
2023-01-31 07:32:16 +0700

    menambahkan Hamburger Menu dan Linked Medsos

--- scss/js/script.js
+++ scss/js/script.js
@@ -1,5 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-menu");
 
-hamburger.addEventListener("click", function () {
+hamburger.addEventListener("click", function (e) {
   hamburger.classList.toggle("active");
+  if (true) {
+    navMenu.classList.toggle("active");
+    console.log(navMenu);
+  }
 });
